06a97ad2b342c3b57b051804b32ffc23df39961d
Roman Sharf <pencilking2002@gmail.com>
2014-11-08 16:42:33 -0500

    Added an initial player model

---
+++ Assets/CharController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+	
+	public speed = 10f;
+	public rotationSPeed = 100f;
+	
+	private float h;
+	private float v;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		h = Input.GetAxis ("Horizontal");
+		v = Input.GetAxis ("Horizontal");
+	}
+}
