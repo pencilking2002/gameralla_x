47ed3f50f6edaa205a9955befbad1bc10362c034
Roman Sharf <pencilking2002@gmail.com>
2014-11-08 21:44:59 -0500

    Created Springing plant prefab
    Location: in Resources/Prefabs/GrassSpringingUp

---
+++ Assets/GrassSpringUp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassSpringUp : MonoBehaviour {
+	
+	public float finalScale = 1.2f;
+	public float duration = 2.0f;
+	
+	public GameObject child;
+	
+	void OnTriggerEnter(Collider collider)
+	{
+		//print(collider.gameObject.tag);
+		LeanTween.scaleX(child, finalScale, duration).setEase(LeanTweenType.easeOutElastic);
+		LeanTween.scaleZ(child, finalScale, duration).setEase(LeanTweenType.easeOutElastic);
+		
+	}
+	
+	
+}
