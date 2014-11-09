4abd21db931c1bc091227fcf886c2c556e5aa34b
Roman Sharf <pencilking2002@gmail.com>
2014-11-08 20:19:06 -0500

    working on fade script

---
+++ Assets/FadeIn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F))
+		{
+			// Fade GO in from 0 to 1, but it doesn't work?
+			LeanTween.alpha(gameObject, 0f, 1f);
+		}
+	}
+	
+	
+}
