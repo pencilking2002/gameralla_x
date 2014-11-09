c74ddb3ea6b618bd7abe06f005a4b1395a192838
Roman Sharf <pencilking2002@gmail.com>
2014-11-09 13:46:18 -0500

    Created Mushroom prefab

---
+++ Assets/Scripts/MushroomSpringUp.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MushroomSpringUp : MonoBehaviour {
+
+	public float finalScaleX = 1.2f;
+	public float finalScaleY = 1.2f;
+	public float finalScaleZ = 1.2f;
+	public float finalPosY = -2.549794f;
+	public float duration = 2.0f;
+	public GameObject child;
+		
+	public void TweenMuchroom()
+	{
+		{
+			//print(collider.gameObject.tag);
+			LeanTween.scaleX(child, finalScaleX, duration).setEase(LeanTweenType.easeInCubic);
+			LeanTween.scaleY(child, finalScaleY, duration).setEase(LeanTweenType.easeInCubic);
+			LeanTween.scaleZ(child, finalScaleZ, duration).setEase(LeanTweenType.easeInCubic);
+			LeanTween.moveLocalY(child, finalPosY, duration).setEase(LeanTweenType.easeInCubic);
+			
+		}
+	}
+	//8.866213
+}
