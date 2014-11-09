01706c4f77e14c2c423160b34a22226edd3be02d
Roman Sharf <pencilking2002@gmail.com>
2014-11-09 14:45:34 -0500

    Created End Scene

---
+++ Assets/Scripts/EndScreenScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScreenScript : MonoBehaviour {
+	public GameObject cameraTarget;
+	private float timer = 4f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > 0){
+			timer -= Time.deltaTime;
+			return;
+		}
+		if (Input.anyKeyDown){
+			cameraTarget.transform.position = new Vector3(0, -9f, 0f);
+			Invoke("LoadGame", 2f);
+			CameraFade.StartAlphaFade( Color.black, false, 2f, 2f);
+		}
+	}
+	
+	void LoadGame(){
+		Application.LoadLevel(1);
+	}
+}
